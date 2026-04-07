3224622173c0472a4d4ebd807a82a436145fe79f
atsumi-web <atsumiwebstudio@gmail.com>
2026-04-07 13:37:11 +0900

    chore: ルート直下の不要な一時ファイル群を削除・整理（作業用スクリプトの退避）

--- tmp_check_links3.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const html = fs.readFileSync('site/client-work/07_youchien/hp/index.html', 'utf8');
-const lines = html.split('\n');
-lines.forEach((l, i) => {
-  if(l.includes('href=\"#\"') || l.includes('href=\"javascript')) {
-    console.log(i + ': ' + l.trim());
-  }
-});
